ae6ba4dbd5a9aa223aeb17dbcf80eb055503f4e3
gabriel2m <gabriel.mendes.rf@gmail.com>
2021-07-14 16:25:08 -0300

    Update layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,23 +18,23 @@
 
 <body class="flex flex-col min-h-screen bg-indigo-50 text-gray-800">
     <header>
-        <nav class="py-10 bg-gray-800">
+        <nav class="py-10 bg-gray-800 custom-row">
 
         </nav>
-        <div class="py-44 bg-green-600">
-            
+        <div class="py-44 bg-green-600 custom-row">
+
         </div>
     </header>
 
-    <main class="flex flex-col py-16 px-6">
+    <main class="py-16 custom-row">
         {{ $content }}
     </main>
 
     <footer class="mt-auto">
-        <div class="py-10 bg-green-800">
+        <div class="py-10 bg-green-800 custom-row">
 
         </div>
-        <div class="py-7 bg-green-600">
+        <div class="py-7 bg-green-600 custom-row">
 
         </div>
     </footer>
