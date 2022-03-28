2bf88d476d3e896e04355f16f3548004234fd9ad
gabriel2m <gabriel2m.contact@gmail.com>
2022-03-28 15:39:31 -0300

    Update node dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,18 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    container: {
-      center: true,
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'ifnmg-green': {
+                    1: '#17882c',
+                    2: '#00420c',
+                    3: '#002907',
+                }
+            },
+        },
     },
-    extend: {
-      textColor: {
-        'violet': colors.violet,
-      },
-      colors: {
-        'blue-gray': colors.blueGray,
-        'ifnmg-green': {
-          1: '#17882c',
-          2: '#00420c',
-          3: '#002907',
-        }
-      }
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 }
