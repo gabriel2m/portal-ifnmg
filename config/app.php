3c0954f6705545a9f97b95d53cc4f03bbb5202c5
gabriel2m <gabriel2m.contact@gmail.com>
2022-07-02 14:09:32 -0300

    Refactoring

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Escape telescope:install to change app.php :
+        // App\Providers\TelescopeServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
 
     ],
